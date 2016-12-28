04961dbc7f171896978ace23190d83a4e7a391ad
Gonçalo Cardoso <goncalo.cb.ferreira@gmail.com>
2016-12-28 17:12:37 +0000

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+module.exports = {
+	entry: './public/js/main.js',
+	output: {
+		path: path.join(__dirname, '/public/js/'),
+		filename: 'app.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: path.join(__dirname, '/public/js/'),
+				loader: 'babel-loader',
+				query: {
+					presets: [ 'es2015', 'stage-2' ]
+				}
+			}
+		]
+	},
+	devtool: 'source-map'
+};
