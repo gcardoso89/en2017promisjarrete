1420367296b729250120eb2fa97a398ca2ea99c1
Gonçalo Cardoso <goncalo.cb.ferreira@gmail.com>
2017-01-05 22:21:11 +0000

    Added random color on start

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+	colors : [ '#ff7376', '#ffc0db', '#7bc9cf', '#ffdc00', '#4f7ffb' ]	
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require( 'express' );
 var router = express.Router();
+var colors = require( '../config' ).colors;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get( '/', function ( req, res, next ) {
+	var randomIndex = Math.floor( Math.random() * colors.length ) + 1;
+	res.render( 'index', { title: 'Express', color: colors[ randomIndex ] } );
+} );
 
 module.exports = router;
