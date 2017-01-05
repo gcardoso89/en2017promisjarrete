b29998ea48ebb82d1b0a0c540426c076377b622a
Gonçalo Cardoso <goncalo.cb.ferreira@gmail.com>
2017-01-05 22:46:04 +0000

    Fixed color issue on random number and add padding on header

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,7 @@ var config = require( '../config' );
 
 /* GET home page. */
 router.get( '/', function ( req, res, next ) {
-
-	var color = config.colorList[ Math.floor( Math.random() * config.colorList.length ) + 1 ];
+	var color = config.colorList[ Math.floor( Math.random() * ( config.colorList.length - 1 ) ) ];
 	var colorName = config.colorMap[ color ];
 	res.render( 'index', { title: 'Express', classColorName: 'color-' + colorName } );
 } );
