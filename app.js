05e98eecf6a729646f9d3fc6681f328f0e75b17a
Gonçalo Cardoso <goncalo.cb.ferreira@gmail.com>
2017-01-05 23:28:28 +0000

    Added new font size to logo

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use( logger( 'dev' ) );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: false } ) );
 app.use( cookieParser() );
-app.use( require( 'node-compass' )( { mode: 'expanded' } ) );
+app.use( require( 'node-compass' )( { logging: true } ) );
 
 app.use( '/', routes );
 
