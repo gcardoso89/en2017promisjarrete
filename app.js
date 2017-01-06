a1abb36f05c9ab3ffac8bacf68156768db16e901
Gonçalo Cardoso <goncalo.cb.ferreira@gmail.com>
2017-01-06 23:52:58 +0000

    Test to fix the style.css generation issue

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ app.set( 'view engine', 'hbs' );
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use( require( 'node-compass' )( { mode: 'expanded', project: __dirname, css: 'public/css', sass: 'compass/' } ) );
 app.use( express.static( path.join( __dirname, 'public' ) ) );
 app.use( logger( 'dev' ) );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: false } ) );
 app.use( cookieParser() );
-app.use( require( 'node-compass' )( { logging: true } ) );
 
 app.use( '/', routes );
 
